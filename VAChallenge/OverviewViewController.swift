05f26293986e1b4eaf4ab08b02b3ff47b2dd570b
Adil BOUGAMZA <Adil.Bougamza@nespresso.com>
2016-12-03 16:57:57 +0000

    show appointments in OverviewViewController

--- VAChallenge/OverviewViewController.swift
+++ VAChallenge/OverviewViewController.swift
@@ -7,22 +7,65 @@
 //
 
 import UIKit
+import SnapKit
 
-class OverviewViewController: UIViewController {
+class OverviewViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    var appointments : [Appointment] = []
+    let cellId :String = "cellId"
+
+    let tableView : UITableView = {
+        let tableView = UITableView(frame: CGRect.zero, style: UITableViewStyle.plain)
+        return tableView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        self.view.backgroundColor = UIColor.blue
+        self.view.backgroundColor = UIColor.white
         // Do any additional setup after loading the view.
 
         self.navigationItem.title = "Overview"
-        
+
+        let addButtonItem :UIBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addButtonAction))
+        self.navigationItem.setRightBarButton(addButtonItem, animated: true)
+
+        self.view.addSubview(tableView)
+        tableView.snp.makeConstraints { (make) in
+            make.edges.equalTo(self.view)
+        }
+
+        self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellId)
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+
+
+        let app1 = Appointment(_beginDate: Date(), _endDate: Date())
+        let app2 = Appointment(_beginDate: Date(), _endDate: Date())
+        appointments += [app1, app2]
+
+    }
+
+    func addButtonAction() {
+        let scheduleVC :ScheduleViewController = ScheduleViewController()
+        self.navigationController?.pushViewController(scheduleVC, animated: true)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return appointments.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellId)!
+        cell.textLabel?.text = "celllllll"
+
+        return cell
+
     }
 
 }
