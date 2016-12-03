4cc5710210976192c78259bccb9d6ac9bfe64592
Adil BOUGAMZA <Adil.Bougamza@nespresso.com>
2016-12-03 14:13:19 +0000

    add clearButton navigation item + set navigation title

--- VAChallenge/ViewController.swift
+++ VAChallenge/ViewController.swift
@@ -13,7 +13,18 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        self.view.backgroundColor = UIColor.red
+        self.view.backgroundColor = UIColor.lightGray
+
+        self.navigationItem.title = "Schedule"
+
+        // add navigation item : clear
+        let clearButtonItem :UIBarButtonItem = UIBarButtonItem(title: "Clear", style: .plain, target: self, action: #selector(self.clearButtonClick))
+        self.navigationItem.setRightBarButton(clearButtonItem, animated: true)
+    }
+
+
+    func clearButtonClick() {
+        print("clear")
     }
 
     override func didReceiveMemoryWarning() {
