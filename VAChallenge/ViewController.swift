349a3fb8c41b06ad7b1c90ef1f44be1bd4db2719
Adil BOUGAMZA <Adil.Bougamza@nespresso.com>
2016-12-03 13:54:17 +0000

    Initial Commit

---
+++ VAChallenge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VAChallenge
+//
+//  Created by Adil BOUGAMZA on 03/12/2016.
+//  Copyright © 2016 Adil Bougamza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
